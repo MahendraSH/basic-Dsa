6f94dfc5c81bb795df9b134ee82c1aac1e40bde2
MahendrSH <maahi2001sh@gmail.com>
2023-02-28 18:28:28 +0530

    some files are of lad i practice

---
+++ wta java lab/file3.java
@@ -0,0 +1,12 @@
+class main{
+    public static void main(String ...args)
+    {
+        int n=Integer.parseInt(args[0]);
+        int m=Integer.parseInt(args[1]);
+        for(int i=1;i<m;i++)
+        {
+            System.out.println(i*n);
+        }
+        
+    }
+}
