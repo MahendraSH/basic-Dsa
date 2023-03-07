3fed5ff6489553625546a96eb309e82bff554f3f
MahendrSH <maahi2001sh@gmail.com>
2023-03-07 20:57:54 +0530

    ass3

---
+++ wta class/assignment programs/ass3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// ass3.java
+//leap year progarm chek is leap or not 
+
+public class ass3 {
+    public static void main(String[] args) {
+        System.out.println("Enter the  year ");
+        Scanner cin=new Scanner(System.in);
+        int n=cin.nextInt();
+        if(n%4==0){
+            if(n%100==0)
+            {
+                if(n%400==0)
+                {
+                    System.out.println("leap year ");
+                }
+                else{
+                    System.out.println("not leap year");
+                }
+            }
+            else{
+                System.out.println("leap year");
+            }
+        }else{
+            System.out.println("not a leap year");
+        }
+    }
+    
+}
