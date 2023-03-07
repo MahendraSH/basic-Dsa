8b7f278ac63847378b603527809fb27de7e86104
MahendrSH <maahi2001sh@gmail.com>
2023-03-07 21:41:02 +0530

    formated output using string class

---
+++ wta class/wtaJava9.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+// wtaJava9.java
+// formated outpurt using  string class
+
+public class wtaJava9 {
+    public static void main(String[] args) {
+        System.out.println("Enter the emp id ,name ,and the salary");
+        Scanner cin = new Scanner(System.in);
+        int id = cin.nextInt();
+        String name = cin.next();
+        float salary = cin.nextFloat();
+        // String s =String.format(name, args)
+
+        String s = String.format(
+                "emp id =%d \n emp name =%s %n emp salary =%f %n", id, name, salary);
+
+                System.out.println(s);
+        cin.close();
+    }
+
+}
