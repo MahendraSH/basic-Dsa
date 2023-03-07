5879dfb5de5b7d8e22a2982a93ccd6b12cbd3a28
MahendrSH <maahi2001sh@gmail.com>
2023-03-07 17:49:47 +0530

    java return stament

---
+++ wta class/wtaJava2.java
@@ -0,0 +1,15 @@
+// wtaJava2.java
+//  return statment in java
+public class wtaJava2{
+    public static void main(String ... args){
+        int n=0;
+        if(n==0)
+        {
+            System.out.println("it is zero before");
+            return ;
+            // System.out.println("it is zero after");
+            // anting after retur is not printed and not executed 
+            //  in most of the jvm it gives error
+        }
+    }
+}
