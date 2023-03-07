a184151147732edecdef8cf85d733e6d1a03bfb5
MahendrSH <maahi2001sh@gmail.com>
2023-03-07 20:48:19 +0530

    ass 2

---
+++ wta class/assignment programs/ass2.java
@@ -0,0 +1,23 @@
+// ass2.java
+//  print the n fibonachi numbers 
+import java.util.Scanner;
+
+public class ass2 {
+    public static void main(String[] args) {
+        Scanner cin  =new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=cin.nextInt();
+        int t1=0;
+        int t2=1;
+        int count=0;
+        while(count<n){
+            System.out.print(t1+" ");
+            int temp=t1+t2;
+            t1=t2;
+            t2=temp;
+            count++;
+          
+        }
+
+    }
+}
