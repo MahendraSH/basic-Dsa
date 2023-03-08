3501cee0dc5ec9dc4d741697044f15b386bf9623
MahendrSH <maahi2001sh@gmail.com>
2023-03-08 14:46:50 +0530

    pro-2

---
+++ wta java lab/day-2 or module-1/prolab2a.java
@@ -0,0 +1,35 @@
+
+// prolab2.java
+// Write a Java Programs to demonstrate the usage of the following:
+	// a.  Java’s Primitive Data Types and Literals
+
+public class prolab2a {
+    public static void main(String[] args) {
+		
+		int n=3289;
+		System.out.println("int -"+n);
+
+		long l=2989434343498L;
+		System.out.println("long int"+l);
+
+		short s =943;
+		System.out.println("short -"+s);
+
+		byte b=94;
+		System.out.println("byte-"+b);
+
+		char c ='c';
+		System.out.println("char -"+c);
+
+		float f =483.4038f;
+		System.out.println("float -"+f);
+
+		double d =489.5308953590598;
+		System.out.println("double -"+d);
+
+		boolean False =false;
+		System.out.println("boolean -"+False);
+		
+		
+	}
+}
