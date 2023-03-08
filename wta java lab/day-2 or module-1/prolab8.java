9cf149beb3c857b47cd085d2c7d92aef061000a4
MahendrSH <maahi2001sh@gmail.com>
2023-03-08 15:54:13 +0530

    pro lab 8

---
+++ wta java lab/day-2 or module-1/prolab8.java
@@ -0,0 +1,10 @@
+public class prolab8 {
+
+    public static void main(String[] args) {
+        System.out.println(
+                "sum ="
+                        + (Integer.parseInt(args[0]) + Integer.parseInt(args[1])));
+    }
+
+    
+}
