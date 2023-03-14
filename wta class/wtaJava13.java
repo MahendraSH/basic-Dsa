48081089f8ad2815c60b8c0b95d6c52244c8260d
MahendrSH <maahi2001sh@gmail.com>
2023-03-14 08:30:28 +0530

    demostartion of sime class and metheons using Person class

--- wta class/wtaJava13.java
+++ wta class/wtaJava13.java
@@ -1,12 +1,19 @@
-//  static class and static and variables
-class Demo{
-    static int n=10;
-    
+// person class exmple 
+// with name and age and take function
 
+public class Person{
+    String name;
+    int age;
+    void takeFunction(){
+        System.out.println("Hello  my name is " + name +" and my age is "+age);
+    }
 }
-public class wtaJava13 {
-   
+
+public class wtaJava13{
     public static void main(String[] args) {
-        
+        Person p1=new Person();
+        p1.name="Mahendra";
+        p1.age=22;
+        p1.takeFunction();
     }
 }
