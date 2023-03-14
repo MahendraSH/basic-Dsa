2579ca1f2309f3c81d738156541926d8c568a9c8
MahendrSH <maahi2001sh@gmail.com>
2023-03-14 19:20:03 +0530

    // instance varible with instance method

---
+++ wta class/wtaJava22.java
@@ -0,0 +1,17 @@
+// instance varible with instance method
+
+class Demo {
+    private int n1 = 10;
+
+    void display() {
+        System.out.println("the numnber is " + n1);
+    }
+}
+
+public class wtaJava22 {
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        d.display();
+
+    }
+}
