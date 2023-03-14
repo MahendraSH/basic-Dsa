7e0db797392f3abe27d1dadb9ddfb44b02a6901d
MahendrSH <maahi2001sh@gmail.com>
2023-03-14 18:36:33 +0530

    create a method with out parmeter and with out return type

---
+++ wta class/wtajava16.java
@@ -0,0 +1,21 @@
+//  create a method with out parmeter and with out return type
+class Demo{
+    private int n1;
+    private int n2;
+    public Demo(int n1, int n2){
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+    void sum()
+    {
+        System.out.println("sum = "+(n1+n2));
+    }
+}
+public class wtajava16 {
+    
+    public static void main(String[] args) {
+        Demo d =new Demo(10,30);
+        d.sum();
+        
+    }
+}
