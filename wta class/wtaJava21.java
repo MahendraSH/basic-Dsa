cb8804eb313bb82bbd39a160cc64b99c3a408ea6
MahendrSH <maahi2001sh@gmail.com>
2023-03-14 19:15:56 +0530

    // static block wirth out mian

---
+++ wta class/wtaJava21.java
@@ -0,0 +1,18 @@
+// static block wirth out mian
+
+//  u can cheet jvm in lower version like his but not heigher versions
+
+public class wtaJava21 {
+    
+static{
+    
+    System.out.println("static block1 ");
+    System.out.println("static block 2");
+    System.out.println("static block 3");
+    System.out.println("static block 4");
+    System.out.println("static block 5");
+    System.out.println("static block 6");
+    System.exit(0);
+}
+
+}
