4c3b74ba8532a11558e4f50ad1fc8ae17afc4ec5
MahendrSH <maahi2001sh@gmail.com>
2023-03-19 19:34:12 +0530

    The final class final virable final method

---
+++ wta java lab/day_3/day3lab3.java
@@ -0,0 +1,38 @@
+package day_3;
+
+final class Demo{
+    Demo(){
+        System.out.println(
+            "These class can not be inherited or extended as its a final class"
+            );
+    }
+
+
+    
+}
+
+class TwoNumbers{
+  final int n1, n2;
+
+    TwoNumbers( int n1, int n2){
+        this.n1 = n1;
+        this.n2=n2;
+    }
+    final int sum(){
+        return (n1 + n2);
+    }
+}
+
+public class day3lab3 {
+    
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        TwoNumbers twoNumbers =new TwoNumbers(19,20);
+        
+        System.out.println("The sum of the two numbers ="+twoNumbers.sum());
+
+
+
+    }
+
+}
