badaf4efdc85fe30e409e44f3abcd1fc7854b512
MahendrSH <maahi2001sh@gmail.com>
2023-03-31 23:28:16 +0530

    sum of two int using args[]

---
+++ wta class/partice/pra11.java
@@ -0,0 +1,11 @@
+package partice;
+
+public class pra11 {
+    public static void main(String[] args) {
+//        sum of int using console
+        System.out.println("Sum ="+
+                ( Integer.parseInt(args[0])+
+                        Integer.parseInt(args[1]) ));
+
+    }
+}
