59b4f2ba40c2152b5ae28c2bf9be308a01e083f3
MahendrSH <maahi2001sh@gmail.com>
2023-03-31 23:11:27 +0530

    find the avg and toatal in array

--- wta class/partice/pra9.java
+++ wta class/partice/pra9.java
@@ -1,2 +1,15 @@
-package partice;public class pra9 {
+package partice;
+
+import java.util.Scanner;
+
+public class pra9 {
+    public static void main(String[] args) {
+        Scanner cin=new Scanner(System.in);
+        System.out.println("enter the uname id and salary");
+        String uname =cin.next();
+        int id = cin.nextInt();
+        float salary = cin.nextFloat();
+        String s= String.format("uname = %s \n id =%d \n salary = %f",uname,id,salary);
+        System.out.println(s);
+    }
 }
