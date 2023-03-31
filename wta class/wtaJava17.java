8541f0db95063b137bd34791ff69e10711d4dba8
MahendrSH <maahi2001sh@gmail.com>
2023-03-31 18:57:26 +0530

    chaged class to democlass

--- wta class/wtaJava17.java
+++ wta class/wtaJava17.java
@@ -1,9 +1,9 @@
 // create a method with out parmeter and with return type
-class Demo {
+class Demo1 {
     private int n1;
     private int n2;
 
-    Demo(int n1, int n2) {
+    Demo1(int n1, int n2) {
         this.n1 = n1;
         this.n2 = n2;
     }
@@ -15,7 +15,7 @@ class Demo {
 
 public class wtaJava17 {
     public static void main(String[] args) {
-        Demo d = new Demo(10, 20);
+        Demo1 d = new Demo1(10, 20);
         System.out.println("The sum = " + d.sum());
     }
 }
