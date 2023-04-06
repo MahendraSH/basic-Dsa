cf1f9b19404d3b7db852ade4e066faecdad7d6a2
MahendrSH <maahi2001sh@gmail.com>
2023-04-06 17:57:49 +0530

    internals -1 progrmams

--- wta class/internalsONe/Q2.java
+++ wta class/internalsONe/Q2.java
@@ -1,2 +1,18 @@
-package internalsONe;public class Q2 {
+package internalsONe;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int x=2;
+        int m =5 & 6;
+        System.out.println(m);
+        int n=m^3;
+        System.out.println(n);
+        System.out.println(n|12);
+        String result= m>n? "good":"bad";
+        System.out.println(result);
+        x=x>>1;
+        System.out.println(x);
+        int y=x<<2;
+        System.out.println(y);
+    }
 }
--- wta class/internalsONe/Q3.java
+++ wta class/internalsONe/Q3.java
@@ -1,2 +1,32 @@
-package internalsONe;public class Q3 {
+package internalsONe;
+
+class A {
+    A(){
+        System.out.println("default A");
+    }
+    A(String s){
+        System.out.println("par A");
+    }
+}
+
+class B extends A {
+    B() {
+        super();
+
+        System.out.println("default B");
+    }
+
+    B(String s) {
+        super(s);
+        new B();
+        System.out.println("parter B");
+    }
+}
+
+
+
+public class Q3 {
+    public static void main(String[] args) {
+        B b2 = new B("djnsud");
+    }
 }
