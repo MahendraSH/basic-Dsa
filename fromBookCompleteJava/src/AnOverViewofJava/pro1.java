7c0157c9ddcb8c46fe629979a0bb953f1eef82b3
MahendrSH <maahi2001sh@gmail.com>
2023-04-19 22:17:36 +0530

    pro1 to pro5

--- fromBookCompleteJava/src/AnOverViewofJava/pro1.java
+++ fromBookCompleteJava/src/AnOverViewofJava/pro1.java
@@ -1,2 +1,7 @@
-package AnOverViewofJava;public class pro1 {
+package AnOverViewofJava;
+
+public class pro1 {
+    public static void main(String[] args) {
+        System.out.println("sample java program ");
+    }
 }
--- fromBookCompleteJava/src/AnOverViewofJava/pro4.java
+++ fromBookCompleteJava/src/AnOverViewofJava/pro4.java
@@ -1,2 +1,10 @@
-package AnOverViewofJava;public class pro4 {
+package AnOverViewofJava;
+//simple for loop
+public class pro4 {
+    public static void main(String[] args) {
+        for(int i=0;i< 10;i++)
+        {
+            System.out.println("this i is :"+i);
+        }
+    }
 }
