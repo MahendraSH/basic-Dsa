280b2b101a733a906d470d70e938251dc896f584
MahendrSH <maahi2001sh@gmail.com>
2023-04-20 14:02:40 +0530

    pack -1 add simeple

---
+++ wta java lab/day_5/pack1/Add.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class Add {
+
+    public   void add(int a ,int b)
+    {
+        System.out.println(" Sum "+(a+b));
+    }
+}
---
+++ wta java lab/day_5/pro1.java
@@ -0,0 +1,9 @@
+import  pack1.Add;
+public class pro1 {
+    public static void main(String[] args) {
+
+        Add a =new Add();
+        a.add(20,20);
+    }
+
+}
